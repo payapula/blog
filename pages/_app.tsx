9b0f681b829b1133f974f2d2f659259b7a7f3c91
payapula <bharathikannank05@gmail.com>
2021-03-15 18:24:55 +0530

    Feature/initial setup (#1)
    * feat(Setup): :art: Removed tailwind and installed chakra ui
    
    File changes
    
    * Added Hamburger Icon for Responsive modal.
    
    * fix: :bug: useMediaQuery on initial Load
    
    Moved themes folder. Added useMediaQuery hook. Fixed initial Load Bug.
    
    -----
    -> Removed Tailwind.
    -> Added Chakra UI.
    -> Custom themes folder
    -> Added dark theme.
    -> Implemented responsive Nav Bar.
    -> useMediaQuery bug resolved. https://github.com/chakra-ui/chakra-ui/issues/3124
    -----

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import { AppProps } from "next/app";
+// import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../styles/theme";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,15 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
-import Post from '../types/post'
+import { Button } from "@chakra-ui/react";
+import React from "react";
+import { Layout } from "../mycomponents/Layout";
+import { Heading } from "@chakra-ui/react";
 
-type Props = {
-  allPosts: Post[]
-}
-
-const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+const Index = () => {
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
-    </>
-  )
-}
+    <Layout>
+      <Heading as="h1">Welcome to Payapula's Site</Heading>
+      <Button colorScheme="green">Sample</Button>
+    </Layout>
+  );
+};
 
-export default Index
-
-export const getStaticProps = async () => {
-  const allPosts = getAllPosts([
-    'title',
-    'date',
-    'slug',
-    'author',
-    'coverImage',
-    'excerpt',
-  ])
-
-  return {
-    props: { allPosts },
-  }
-}
+export default Index;
