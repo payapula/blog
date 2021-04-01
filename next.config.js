efdbdd2ec23ea4beee59e926e4b80dc718302bbf
Payapula <bharathikannank05@gmail.com>
2021-04-01 13:55:10 +0530

    Added Bundle Analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withBundleAnalyzer({
     images: {
         domains: ['source.unsplash.com']
     }
-};
+});
