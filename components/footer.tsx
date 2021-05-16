ba350412bac4d342fa2faba077d3878e939a8123
Payapula <bharathikannank05@gmail.com>
2021-05-16 14:31:06 +0530

    Added Links to Footer. Mail ID Copy to Clipboard. Added toast to let user know about email id in clipboard. Edited the resume to include updated email address.

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,24 @@
-import { Box, Flex, Text, HStack, CSSObject, Icon, useColorModeValue } from '@chakra-ui/react';
-import { FaFacebookF } from 'react-icons/fa';
+import {
+    Box,
+    Flex,
+    Text,
+    HStack,
+    CSSObject,
+    Icon,
+    useColorModeValue,
+    Link,
+    Button,
+    useToast
+} from '@chakra-ui/react';
 import { FaTwitter } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import { IconType } from 'react-icons';
 import { ReactElement } from 'react';
+import { SiGmail } from 'react-icons/si';
 
 function Footer(): ReactElement {
     const postsBackgroundColor = useColorModeValue('#EDF7FA', 'gray.800');
+    const toast = useToast();
     return (
         <Box
             height="150px"
@@ -22,9 +34,36 @@ function Footer(): ReactElement {
                 direction="column"
                 mt="10">
                 <HStack justify="space-between" w="200px" mt="5">
-                    <SocialIcons icon={FaFacebookF} hover={{ color: '#4267B2' }} />
-                    <SocialIcons icon={FaTwitter} hover={{ color: '#1DA1F2' }} />
-                    <SocialIcons icon={FaLinkedin} hover={{ color: '#0e76a8' }} />
+                    <Button
+                        width={0}
+                        height={'auto'}
+                        background="transparent"
+                        _hover={{
+                            background: 'transparent'
+                        }}
+                        _active={{
+                            background: 'transparent'
+                        }}
+                        onClick={() => {
+                            navigator.clipboard.writeText('bharathikannanv@gmail.com');
+                            toast({
+                                title: 'Email ID Copied!',
+                                position: 'top-left',
+                                description:
+                                    'Email Address copied to clipboard. Paste it in your favorite mail client to send out an email!',
+                                status: 'success',
+                                duration: 5000,
+                                isClosable: true
+                            });
+                        }}>
+                        <SocialIcons icon={SiGmail} hover={{ color: '#ea4335' }} />
+                    </Button>
+                    <Link href="https://twitter.com/bharathispeaks" isExternal>
+                        <SocialIcons icon={FaTwitter} hover={{ color: '#1DA1F2' }} />
+                    </Link>
+                    <Link href="https://linkedin.com/in/bharathi-kannan-80968170" isExternal>
+                        <SocialIcons icon={FaLinkedin} hover={{ color: '#0e76a8' }} />
+                    </Link>
                 </HStack>
                 <Text>Opensourced on Github ❤️</Text>
             </Flex>
