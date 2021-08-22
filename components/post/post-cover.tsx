47d98f35a64a36b29f1504cc013e0ee5097c71bc
Payapula <bharathikannank05@gmail.com>
2021-08-22 18:33:43 +0530

    Added `plaiceholder` package to dynamically add blur64 to next/image (For Post covers).

--- components/post/post-cover.tsx
+++ components/post/post-cover.tsx
@@ -1,4 +1,4 @@
-import Cover from 'types/cover';
+import Cover, { PlaiceHolderProps } from 'types/cover';
 import { ReactElement } from 'react';
 import { Text, Box } from '@chakra-ui/react';
 import NextLink from 'next/link';
@@ -7,16 +7,25 @@ import Image from 'next/image';
 
 interface PostCoverProps {
     cover: Cover;
+    plaiceHolder: PlaiceHolderProps;
 }
-function PostCover({ cover }: PostCoverProps): ReactElement {
+
+/**
+ * Uses Image Blur from plaicehodler
+ * https://plaiceholder.co/docs/usage#base64
+ * https://github.com/vercel/next.js/discussions/26168#discussioncomment-877038
+ *
+ * until nextjs supports automatic blur from `require` imports
+ * https://nextjs.org/docs/basic-features/image-optimization#image-imports
+ * @returns {ReactElement}
+ */
+function PostCover({ cover, plaiceHolder }: PostCoverProps): ReactElement {
     return (
         <Box mt={8}>
             <Image
-                src={cover.src}
-                alt={cover.alt}
-                width={1400}
-                height={800}
-                priority
+                {...plaiceHolder.img}
+                blurDataURL={plaiceHolder.base64}
+                placeholder="blur"
                 objectFit="contain"
             />
             <Text align="center">
