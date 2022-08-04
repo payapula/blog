61a3fd801aca7e48972c9727d7e43f87113990c4
payapula <bharathikannank05@gmail.com>
2022-08-04 12:32:29 +0530

    - First pass at updating packages

--- components/quotes.tsx
+++ components/quotes.tsx
@@ -31,8 +31,7 @@ const quoteData: QuoteData = [
         author: 'Bertram Gilfoyle'
     },
     {
-        text:
-            'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        text: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
         author: 'Martin Fowler'
     },
     {
@@ -40,8 +39,7 @@ const quoteData: QuoteData = [
         author: 'Steve Jobs'
     },
     {
-        text:
-            'Whether you want to uncover the secrets of the universe, or you just want to pursue a career in the 21st century, basic computer programming is an essential skill to learn.',
+        text: 'Whether you want to uncover the secrets of the universe, or you just want to pursue a career in the 21st century, basic computer programming is an essential skill to learn.',
         author: 'Stephen Hawking'
     },
     {
@@ -50,21 +48,8 @@ const quoteData: QuoteData = [
     }
 ];
 
-/**
- * Returns a random integer between min (inclusive) and max (inclusive).
- * Excellent explanation - https://stackoverflow.com/a/1527820
- * @param min
- * @param max
- * @returns random number
- */
-function getRandomInt(min = 0, max = quoteData.length - 1): number {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 export const Quotes = (): JSX.Element => {
-    const [quoteNo, setQuoteNo] = React.useState(getRandomInt());
+    const [quoteNo, setQuoteNo] = React.useState(1);
 
     const toggleQuotes = (request) => {
         let nextQuote;
