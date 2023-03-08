50d1a3af024b8e0ea658d35fbeafa6438dec1c56
payapula <bharathikannank05@gmail.com>
2023-03-08 14:50:47 +0530

    - Added Quiz State with Multiple Questions. - Added Separator betweek Choises. - Added Navigation between Questions. - Final Card to Show Results.

--- components/mdx/components/quiz.tsx
+++ components/mdx/components/quiz.tsx
@@ -3,17 +3,12 @@ import { MDXComponents } from './index';
 import { Children } from 'react';
 import { Box, Radio, RadioGroup, Container, Flex, Button } from '@chakra-ui/react';
 
-function Question({ children, number }) {
-    return (
-        <div>
-            <p>Number {number}</p>
-            {children}
-        </div>
-    );
+function Question({ children }) {
+    return <div>{children}</div>;
 }
 
 // Should manage individual question / answer state
-function QuestionSet({ children }) {
+function QuestionSet({ children, incrementValidAnswer }) {
     const [answerSubmitted, setAnswerSubmitted] = React.useState(false);
     const [value, setValue] = React.useState('');
     const validAnswer = React.useRef<string>();
@@ -26,7 +21,11 @@ function QuestionSet({ children }) {
             validAnswer.current = index.toString();
         }
         return (
-            <Radio size="md" value={index.toString()}>
+            <Radio
+                size="md"
+                value={index.toString()}
+                isDisabled={answerSubmitted}
+                alignItems="baseline">
                 {child.props.children}
             </Radio>
         );
@@ -39,6 +38,11 @@ function QuestionSet({ children }) {
         choiseElements
     });
 
+    function submitAnswer() {
+        validAnswer.current === value && incrementValidAnswer();
+        setAnswerSubmitted(true);
+    }
+
     const isCorrect = validAnswer.current === value;
 
     return (
@@ -49,10 +53,16 @@ function QuestionSet({ children }) {
             borderRadius="4"
             border={'1px'}>
             {Question}
-            {ChoisesWithAdditionalProps}
-            <Button onClick={() => setAnswerSubmitted(true)} disabled={!value}>
-                Submit
-            </Button>
+            <Flex as="form" onSubmit={submitAnswer} direction="column">
+                {ChoisesWithAdditionalProps}
+                <Button
+                    mt="20px"
+                    onClick={submitAnswer}
+                    disabled={!value || answerSubmitted}
+                    type="submit">
+                    Submit
+                </Button>
+            </Flex>
             {answerSubmitted ? isCorrect ? <div>Correct</div> : <p>Incorrect!</p> : null}
             {answerSubmitted ? Answer : null}
         </Flex>
@@ -64,18 +74,75 @@ function Choise({ children }) {
 }
 
 function Choises({ children, value, setValue, choiseElements }) {
+    const totalChoises = Children.count(choiseElements);
+
+    const choiseWithSeparator = [];
+    Children.forEach(choiseElements, (child, index) => {
+        choiseWithSeparator.push(child);
+        if (totalChoises - 1 !== index) {
+            choiseWithSeparator.push(<hr key={index} />);
+        }
+    });
+
     return (
         <RadioGroup name="form-name" onChange={setValue} value={value}>
-            <Flex direction={'column'}>{choiseElements}</Flex>
+            <Flex direction={'column'}>{choiseWithSeparator}</Flex>
         </RadioGroup>
     );
 }
 
-const Dummy = ({ isAnswer = false, ...rest }): React.ReactElement => <div {...rest} />;
+const Dummy = (props): React.ReactElement => <div {...props} />;
+
+type QuizState = 'idle' | 'playing' | 'over';
 
 // Should manage all question and answer state: Should be giving final results
 function Card({ children }: { children: React.ReactNode }) {
-    return <Container maxW={'700px'}>{children}</Container>;
+    const [quizState, setQuizState] = React.useState<QuizState>('idle');
+    const [questionNo, setQuestionNo] = React.useState(1);
+    const [totalValidAnswer, setTotalValidAnswer] = React.useState(0);
+
+    const totalQuestions = Children.count(children);
+
+    function incrementValidAnswer() {
+        setTotalValidAnswer((number) => number + 1);
+    }
+
+    const QuestionSetWithAddedProps = React.cloneElement(children[questionNo - 1], {
+        incrementValidAnswer,
+        /** key prop is neccessary to tell react that this is brand new instance */
+        key: questionNo - 1
+    });
+
+    const isFinalQuestion = totalQuestions === questionNo;
+
+    if (quizState === 'over') {
+        const score = Math.round((totalValidAnswer / totalQuestions) * 100);
+
+        return (
+            <Container maxW={'700px'}>
+                Total Questions: {totalQuestions}
+                Correct Answers: {totalValidAnswer}
+                Score: {score}
+            </Container>
+        );
+    }
+
+    return (
+        <Container maxW={'700px'}>
+            Question {questionNo}
+            {QuestionSetWithAddedProps}
+            <Button
+                onClick={() => {
+                    if (isFinalQuestion) {
+                        return setQuizState('over');
+                    }
+
+                    setQuestionNo((n) => n + 1);
+                }}>
+                {isFinalQuestion ? 'Submit' : 'Next'}
+            </Button>
+        </Container>
+    );
 }
 
 export const MDXQuizComponents = {
