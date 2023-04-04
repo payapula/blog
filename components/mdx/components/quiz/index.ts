1fd4a7dac0d27496be3b7dd04f27fd1e4187cb61
payapula <bharathikannank05@gmail.com>
2023-04-04 11:57:22 +0530

    Added Playground dev mode.
    - To view all the questions in same view.

--- components/mdx/components/quiz/index.ts
+++ components/mdx/components/quiz/index.ts
@@ -1,12 +1,16 @@
 import { MDXComponents } from '../index';
-
 import { Playground } from './playground';
+import { Playground as DevPlayground } from './playground_dev';
 import { Slot } from './slot';
 import { QuestionSet } from './question-set';
 import { Choises } from './choises';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const PlayGroundComp = process.env.NODE_ENV === 'development' ? DevPlayground : Playground;
+
 export const MDXQuizComponents = {
     ...MDXComponents,
+    // Playground: PlayGroundComp,
     Playground,
     Question: Slot,
     QuestionSet,
