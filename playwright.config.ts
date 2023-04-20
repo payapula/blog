eeb3c13df94524afe7ed1d642912d1b24ee7a2f4
payapula <bharathikannank05@gmail.com>
2023-04-20 20:29:07 +0530

    Feature/Playwright test setup (#78)
    * Added base code with pw extension
    
    * Basic tests added
    
    * - Deleted all examples, matched files specific to browser engines
    
    * Configuration Updates, Added Test cases for Mobile view
    
    * Revert change to history
    
    * Moved SuperLinter Workflow to Archived

---
+++ playwright.config.ts
@@ -0,0 +1,73 @@
+import { defineConfig, devices } from '@playwright/test';
+import siteConfig from 'configs/site-configs';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+const IS_DEV_MODE = false;
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+    testDir: './tests',
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    /* Opt out of parallel tests on CI. */
+    workers: process.env.CI ? 1 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: process.env.CI ? 'github' : IS_DEV_MODE ? 'html' : 'list',
+    ...(IS_DEV_MODE
+        ? {
+              webServer: {
+                  command: 'npm run dev',
+                  port: 3001
+              }
+          }
+        : {}),
+    use: {
+        // headless: false,
+        baseURL: IS_DEV_MODE ? 'http://localhost:3001/' : siteConfig.general.siteUrl,
+        trace: IS_DEV_MODE ? 'on' : 'on-first-retry'
+    },
+
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+            testIgnore: /mobile.*(test|spec)\.(js|ts|mjs)/
+        },
+
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+            testIgnore: /(mobile|chrome).*(test|spec)\.(js|ts|mjs)/
+        },
+
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+            testIgnore: /(mobile|chrome).*(test|spec)\.(js|ts|mjs)/
+        },
+
+        /* Test against mobile viewports. */
+        {
+            name: 'Mobile Chrome',
+            use: { ...devices['Pixel 5'] },
+            testMatch: /mobile.*(test|spec)\.(js|ts|mjs)/
+        },
+        {
+            name: 'Mobile Safari',
+            use: { ...devices['iPhone 12'] },
+            testMatch: /mobile.*(test|spec)\.(js|ts|mjs)/
+        }
+    ]
+});
