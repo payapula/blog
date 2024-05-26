4dff6c3ed5da7871aa0d356a9b58c987c577f9aa
payapula <bharathikannank05@gmail.com>
2024-05-26 20:14:41 +0530

    Title, Avatar and Resume Updates  #86 (#87)

--- tests/homepage/chrome.spec.ts
+++ tests/homepage/chrome.spec.ts
@@ -15,14 +15,14 @@ test.describe('Chrome Desktop Only', () => {
 
         // Ensure the experience cards are not yet visible
         await expect(
-            page.getByRole('heading', { name: 'Senior Associate, Experience Technology' })
+            page.getByRole('heading', { name: 'Lead Experience Engineer' })
         ).not.toBeVisible();
 
         // Scroll down to kick in Experience Cards
         await page.mouse.wheel(0, 2000);
 
         const firstSetOfExperience = [
-            page.getByRole('heading', { name: 'Senior Associate, Experience Technology' }),
+            page.getByRole('heading', { name: 'Lead Experience Engineer' }),
             page.getByRole('heading', { name: 'Software Engineer' }),
             page.getByRole('heading', { name: 'Technical Lead IV' })
         ];
