b8644843671695b556beb62754e7b1033646c1d6
payapula <bharathikannank05@gmail.com>
2024-06-09 23:10:35 +0530

    Added pre-commit hook to generate timestamps when any files inside post directory is updated

---
+++ scripts/generateTs.js
@@ -0,0 +1,18 @@
+var gitDateExtractor = require('git-date-extractor');
+
+gitDateExtractor.getStamps(
+    {
+        onlyIn: '_posts',
+        outputToFile: true,
+        outputFileName: 'timestamps.json',
+        /**
+         * To work along with pre-commit hook
+         *
+         * https://www.npmjs.com/package/git-date-extractor#automating-the-check-in-of-the-timestamp-file-into-version-control-git-add
+         */
+        outputFileGitAdd: true
+    },
+    (stamps) => {
+        console.log('Generating Timestamps', stamps);
+    }
+);
