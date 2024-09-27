f04f9fe0b3e2b84d323ed8e814abf6fd2d0aa750
payapula <bharathikannank05@gmail.com>
2024-09-27 10:42:46 +0530

    ✨ Replace chakra input with native input (#115)

--- components/tailwind/button.tsx
+++ components/tailwind/button.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/lib/utils';
+
 interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
     children: React.ReactNode;
     className?: string;
@@ -6,7 +8,10 @@ interface ButtonProps extends React.ComponentPropsWithoutRef<'button'> {
 export function Button({ children, className = '', ...props }: ButtonProps) {
     return (
         <button
-            className={`focus:focus-outline flex h-10 min-w-10 items-center justify-center rounded bg-gray-200 pe-4 ps-4 dark:bg-gray-700 ${className}`}
+            className={cn(
+                `focus:focus-outline flex h-10 min-w-10 items-center justify-center rounded bg-gray-200 pe-4 ps-4 dark:bg-gray-700`,
+                className
+            )}
             {...props}>
             {children}
         </button>
