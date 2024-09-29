2fb564fb6c842c6513bbfa10f226d98602f9bacc
payapula <bharathikannank05@gmail.com>
2024-09-29 13:07:21 +0530

    Replace link in MDX with tailwind

--- components/mdx/components/index.tsx
+++ components/mdx/components/index.tsx
@@ -1,10 +1,10 @@
 import { Alert, Box, Heading, chakra, Center, Code as ChakraCode } from '@chakra-ui/react';
 import { Code, preToCodeBlock } from './code';
-import { ChakraHeadingLink, ChakraMDXLink } from 'components/chakra-link';
 // import { ChakraNextImage } from 'components/chakra-next-image';
 import NextImage from 'next/image';
 import { ReactElement } from 'react';
 import { QuizHighlight } from './quiz-highlight';
+import { TWHeadingLink, TWMDXLink } from '@/components/tailwind/link';
 
 // Components provided by Chakra UI https://github.com/chakra-ui/chakra-ui/blob/main/website/src/components/mdx-components.tsx
 
@@ -36,9 +36,9 @@ const Pre = (props) => <chakra.div my="2em" borderRadius="sm" {...props} />;
 
 const MDXComponents = {
     h1: (props): ReactElement => <Heading {...props} />,
-    h2: (props) => <ChakraHeadingLink {...props} />,
-    h3: (props) => <ChakraHeadingLink as="h3" {...props} />,
-    h4: (props) => <ChakraHeadingLink as="h4" {...props} />,
+    h2: (props) => <TWHeadingLink {...props} />,
+    h3: (props) => <TWHeadingLink as="h3" {...props} />,
+    h4: (props) => <TWHeadingLink as="h4" {...props} />,
     strong: (props): ReactElement => <Box as="strong" fontWeight="extrabold" {...props} />,
     pre: (preProps) => {
         // Refer Kent C Dodds Implementation below
@@ -75,7 +75,7 @@ const MDXComponents = {
             {...props}
         />
     ),
-    a: ChakraMDXLink,
+    a: TWMDXLink,
     p: (props): ReactElement => <chakra.p {...props} />,
     ul: (props): ReactElement => <chakra.ul {...props} />,
     ol: (props): ReactElement => <chakra.ol {...props} />,
--- components/tailwind/heading.tsx
+++ components/tailwind/heading.tsx
@@ -1,12 +1,18 @@
 /**
  * Tailwind equivalent of Chakra Heading component
  */
-interface HeadingProps {
+export type HeadingProps = {
     children: React.ReactNode;
-}
+    as: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+} & React.HTMLAttributes<HTMLHeadingElement>;
 
 export const defaultHeadingStyles = 'text-3xl font-bold md:text-4xl';
 
-export function Heading({ children }: HeadingProps) {
-    return <h2 className={defaultHeadingStyles}>{children}</h2>;
+export function Heading({ children, as, ...props }: HeadingProps) {
+    const Comp = as ?? 'h2';
+    return (
+        <Comp className={defaultHeadingStyles} {...props}>
+            {children}
+        </Comp>
+    );
 }
