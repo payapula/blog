1fd98d5050280657c08ec2dce126253c1e099918
payapula <bharathikannank05@gmail.com>
2024-12-01 13:39:49 +0530

    Initial commit to inlude endorements

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,7 @@ import { History } from 'components/tailwind/history';
 import { GetStaticProps } from 'next';
 import { getAllPosts } from 'lib/api';
 import Post from 'types/post';
+import TestimonialsSection from '@/components/testimonials-section';
 
 interface IndexProps {
     recentPosts: Post[];
@@ -22,6 +23,7 @@ const Index = ({ recentPosts }: IndexProps): ReactElement => {
             </Head>
             <HeroContent />
             <RecentPosts recentPosts={recentPosts} />
+            <TestimonialsSection />
             <Quotes />
             <Technologies />
             <History />
